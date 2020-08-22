e6d5371b5b4f016c37ce445261233730d1a81b8a
Jayesh Anand <anandjayesh.qa@gmail.com>
2020-08-22 03:05:47 +0100

    Jar file created

--- src/test/java/baseTest/BaseTest.java
+++ src/test/java/baseTest/BaseTest.java
@@ -1,19 +1,44 @@
 package baseTest;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BaseTest {
 
     protected WebDriver driver;
 
     @BeforeTest
-    public void setup() {
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+    public void setup() throws MalformedURLException {
+        /*System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         this.driver = new ChromeDriver();
-        this.driver.manage().window().maximize();
+        this.driver.manage().window().maximize();*/
+
+        // BROWSER => chrome / firefox
+        // HUB_HOST => localhost / 10.11.12.13 / hostname
+
+        String host = "devops.local";
+        DesiredCapabilities dc;
+
+        if(System.getProperty("BROWSER") != null &&
+                System.getProperty("BROWSER").equalsIgnoreCase("firefox")) {
+            dc = DesiredCapabilities.firefox();
+        } else {
+            dc = DesiredCapabilities.chrome();
+        }
+
+        if(System.getProperty("HUB_HOST") != null) {
+            host = System.getProperty("HUB_HOST");
+        }
+
+        String completeUrl = "http://" + host + ":4444/wd/hub";
+        this.driver = new RemoteWebDriver(new URL(completeUrl), dc);
+
     }
 
     @AfterTest
